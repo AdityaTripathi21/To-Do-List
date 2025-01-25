79609a5d2c8bd50e66828c526cf69b146e897109
AdityaTripathi21 <atriptrip21@gmail.com>
2025-01-24 23:58:58 -0500

    add frontend api call to register

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -4,19 +4,64 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Register() {
+    const [username, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isSuccess, setIsSuccess] = useState(false);
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/api/users/register', {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({username, password})
+            });
+            const data = await response.json();
+
+            if (response.ok) {
+                setError('');
+                setIsSuccess(true);
+                setTimeout(() => {
+                    navigate('/');
+                }, 1000);
+            }
+            else {
+                setError(data.message);
+                setIsSuccess(false);
+            }
+        }
+        catch {
+            console.error('Error during registration:', error);
+            setIsSuccess(false);
+            setError('Something went wrong. Please try again later.');
+        }
+    };
+
     return(
         <div className={styles.register}>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleRegister}>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username"/>
+            <input type="text" id="username" name="username" onChange={(e) => setUser(e.target.value)}/>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password"/>
+            <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
             <button type="submit">Register</button>
             </form>
             <p>
                 Already have an account? <Link to="/login">Login!</Link>
             </p>
+
+            {error && <p className={styles.error}>{error}</p>} 
+            {isSuccess && (
+                <p className={styles.success}>
+                    Registration successful! Redirecting to the homepage...
+                </p>
+            )}
         </div>
     );
 }
